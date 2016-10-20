9a70d55b9de463218e2841034ac67022022bd3f2
Anish <anishbhobe@hotmail.com>
2016-10-20 18:26:42 +0530

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  InfernoEngine
+//
+//  Created by Anish Bhobe on 10/20/16.
+//  Copyright © 2016 Anish Bhobe. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
