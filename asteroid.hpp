8f1137deb7723191285f6d40575071e115fe36d0
ronakharkhani <r.r.n.harkhani@gmail.com>
2016-12-10 22:19:33 +0530

    asteroid class created

---
+++ asteroid.hpp
@@ -0,0 +1,27 @@
+//--------------------------------------------------------------------------------
+//
+//  asteroid.hpp
+//  InfernoEngine
+//
+//  Created by Ronak Harkhani on 12/10/16.
+//  Copyright © 2016 Anish Bhobe. All rights reserved.
+//
+//--------------------------------------------------------------------------------
+
+
+#ifndef asteroid_hpp
+#define asteroid_hpp
+
+#include "Projectile.hpp"
+#include "vector3.hpp"
+
+class Asteroid : public Projectile{
+
+
+
+
+
+};
+
+#endif // !
+
