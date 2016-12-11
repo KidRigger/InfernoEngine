8819246ca32a91c370253942a4ad3db6f6f366f9
ronakharkhani <r.r.n.harkhani@gmail.com>
2016-12-12 03:21:37 +0530

    ufo  added (not working)

--- ufo.cpp
+++ ufo.cpp
@@ -10,4 +10,19 @@
 
 #include "ufo.hpp"
 
-Ufo:Ufo(Vector3 pos_vec, )
+Ufo::Ufo(Vector3 pos_vec, float radius):pos_vec(pos_vec), radius(radius) {
+	;
+}
+
+float Ufo::GetRadius() {
+	return radius;
+}
+
+void Ufo::Draw() {
+	for (int i = 0; i < 7; ++i) {
+		Renderer::draw_line(radius*shape[i], radius*shape[i + 1], 3);
+	}
+	Renderer::draw_line(radius*shape[7], radius*shape[2], 3);
+	Renderer::draw_line(radius*shape[6], radius*shape[3], 3);
+
+}
--- ufo.hpp
+++ ufo.hpp
@@ -13,6 +13,10 @@
 
 #include "renderer.hpp"
 
+Vector3 shape[8] = { Vector3(-0.375, 0.75), Vector3(0.375, 0.75), Vector3(0.75, 0.375),
+					 Vector3(1.5, 0), Vector3(0.875, -0.375), Vector3(-0.875, -0.375),
+					 Vector3(-1.5, 0), Vector3(-0.75, 0.375) };
+
 class Ufo {
 	Vector3 pos_vec;
 	float radius;
@@ -20,7 +24,7 @@ class Ufo {
 public:
 	Ufo(Vector3 pos_vec, float radius = 0);
 	void Draw();
-	void GetRadius();
+	float GetRadius();
 };
 
 #endif // !ufo_hpp
