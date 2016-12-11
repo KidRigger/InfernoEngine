393d1fb17d910d7b009c941dc63411039ea035c7
Anish <anishbhobe@hotmail.com>
2016-12-12 01:26:20 +0530

    formatting done

--- asteroid.cpp
+++ asteroid.cpp
@@ -13,13 +13,13 @@
 #include <ctime>
 
 const static Vector3 shape[3][10] = {
-	{Vector3(0.5,1.5), Vector3(1.5,1), Vector3(1,0.5), Vector3(2.5,0),
+	{   Vector3(0.5,1.5), Vector3(1.5,1), Vector3(1,0.5), Vector3(2.5,0),
 		Vector3(1.5,-1), Vector3(0.5,-1.5), Vector3(-1,-1), Vector3(-1,0),
 		Vector3(-1.5,1), Vector3(-0.5,1)},
-	{Vector3(0,2), Vector3(2,1), Vector3(2,-0.5), Vector3(1.5,-1.5),
+	{   Vector3(0,2), Vector3(2,1), Vector3(2,-0.5), Vector3(1.5,-1.5),
 		Vector3(0,-2), Vector3(-1.5,-1), Vector3(-1,-1), Vector3(-2,1),
 		Vector3(-0.5,0.5), Vector3(-1,1.5) },
-	{Vector3(-0.5,2), Vector3(0.5,1.5), Vector3(1.5,2), Vector3(1,0.5),
+	{   Vector3(-0.5,2), Vector3(0.5,1.5), Vector3(1.5,2), Vector3(1,0.5),
 		Vector3(1.5,-0.5), Vector3(1.5,-1.5), Vector3(0.5,-1), Vector3(0,-2),
 		Vector3(-1.5,0), Vector3(-1.5,1.5)} };
 
