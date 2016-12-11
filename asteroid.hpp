39e6330e5836160ac9dbc570b369e75d99bdc29b
ronakharkhani <r.r.n.harkhani@gmail.com>
2016-12-11 20:20:09 +0530

    Text class created

--- asteroid.hpp
+++ asteroid.hpp
@@ -14,11 +14,25 @@
 
 #include "Projectile.hpp"
 #include "vector3.hpp"
+#include "renderer.hpp"
 
 class Asteroid : public Projectile{
 
+	Vector3 pos_vec;
+	float radius;
+	float angle;
+	int shape_id;
+	   
+	Vector3 shape[3][10] = {Vector3(0.5,1.5), Vector3(1.5,1), Vector3(1,0.5), Vector3(2.5,0), Vector3(1.5,-1), Vector3(0.5,-1.5), Vector3(-1,-1), Vector3(-1,0), Vector3(-1.5,1), Vector3(-0.5,1),
+							Vector3(0,2), Vector3(2,1), Vector3(2,-0.5), Vector3(1.5,-1.5), Vector3(0,-2), Vector3(-1.5,-1), Vector3(-1,-1), Vector3(-2,1), Vector3(-0.5,0.5), Vector3(-1,1.5),
+							Vector3(-0.5,2), Vector3(0.5,1.5), Vector3(1.5,2), Vector3(1,0.5), Vector3(1.5,-0.5), Vector3(1.5,-1.5), Vector3(0.5,-1), Vector3(0,-2), Vector3(-1.5,0), Vector3(-1.5,1.5) };
+	
+	Vector3 new_shape[10];
 
-
+	Asteroid(const Vector3& pos_vec, float radius = 0);
+	void Draw();
+	Vector3 GetPosition(void);
+	float GetRadius(void);
 
 
 };
---
+++ text.cpp
@@ -0,0 +1,7 @@
+//
+//  text.hpp
+//  InfernoEngine
+//
+//  Created by  Ronak Harkhani on 12/10/16.
+//  Copyright © 2016 Anish Bhobe. All rights reserved.
+//
